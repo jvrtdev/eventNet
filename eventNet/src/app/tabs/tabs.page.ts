2cd32420328adddecf80a14e232268009d988611
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-08-15 21:58:41 -0300

    feat: add tabs component navigation

---
+++ eventNet/src/app/tabs/tabs.page.ts
@@ -0,0 +1,49 @@
+import { Component, EnvironmentInjector, inject } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  calendarOutline,
+  chatbubblesOutline,
+  homeOutline,
+  ticketOutline,
+} from 'ionicons/icons';
+import { HeaderComponent } from 'src/components/header/header.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  standalone: true,
+  imports: [
+    IonContent,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonLabel,
+    
+    HeaderComponent,
+
+  ],
+})
+export class TabsPage {
+  public environmentInjector = inject(EnvironmentInjector);
+
+  constructor() {
+    addIcons({
+      calendarOutline,
+      ticketOutline,
+      homeOutline,
+      chatbubblesOutline,
+    });
+  }
+}
---
+++ eventNet/src/app/tabs/tabs.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+export const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tickets',
+        loadComponent: () =>
+          import('./components/tickets/tab1.page').then((m) => m.Tab1Page),
+      },
+      {
+        path: 'feed',
+        loadComponent: () =>
+          import('./components/feed/tab2.page').then((m) => m.Tab2Page),
+      },
+      {
+        path: 'events',
+        loadComponent: () =>
+          import('./components/events/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/tickets',
+    pathMatch: 'full',
+  },
+];
