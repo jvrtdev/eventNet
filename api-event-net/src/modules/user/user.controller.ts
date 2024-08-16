0845c716b7f3de6d4cae094f25f00f307033de1c
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-08-16 20:30:05 -0300

    feat: noovo CRUD do user

---
+++ api-event-net/src/modules/user/user.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User as UserModel } from '@prisma/client';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserModel> {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<UserModel[]> {
+    return this.userService.users({});
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id });
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id });
+  }
+}
