a972cc87ebf16e8e596791b2ba65cc446548173e
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-08-21 22:58:50 -0300

    refactor: removendo import de arquivo deletado pt2

--- eventNet/src/app/tabs/components/feed/tab2.page.ts
+++ eventNet/src/app/tabs/components/feed/tab2.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class Tab2Page {
 
