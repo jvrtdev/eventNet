162e6db30f5c36f7513c3920761b1bb4beeb8d44
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-08-22 20:07:20 -0300

    feat: add module chat websocket on app module

--- api-event-net/src/app.module.ts
+++ api-event-net/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { ChatGateway } from './websocket/chat/chat.gateway';
+import { ChatModule } from './websocket/chat/chat.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ChatModule],
   controllers: [],
-  providers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
