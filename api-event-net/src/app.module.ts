8a106d015c0b7f75e621f94071ae805abd9fdec6
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-08-23 16:33:29 -0300

    feat: add chat websocket

--- api-event-net/src/app.module.ts
+++ api-event-net/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { ChatGateway } from './websocket/chat/chat.gateway';
-import { ChatModule } from './websocket/chat/chat.module';
 
 @Module({
-  imports: [UserModule, ChatModule],
+  imports: [UserModule],
   controllers: [],
   providers: [ChatGateway],
 })
--- api-event-net/src/websocket/chat/chat.gateway.ts
+++ api-event-net/src/websocket/chat/chat.gateway.ts
@@ -1,14 +1,27 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway({ cors: true})
-export class ChatGateway {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server
+  private logger: Logger = new Logger("ChatGateway")
 
-  @WebSocketServer()
-  server: Server;
-  
-  @SubscribeMessage('message')
-  handleMessage(client: Socket, message: {sender: string, text: string}): void {
-    this.server.emit('message', message)
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('msgToClient', payload, client.id)
   }
+
+  afterInit(server: Server){
+    this.logger.log('init');
+  }
+
+  handleConnection(client: Socket){
+    this.logger.log(`Client connected: ${client.id}`)
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`)
+  }
+
 }
