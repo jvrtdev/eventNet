9e3ad3b6e2de25d2529b79ff98c3641e4881affb
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-03 22:44:07 -0300

    feat: add conversation, participant modules on appModule

--- api-event-net/src/app.module.ts
+++ api-event-net/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
 import { MessageModule } from './modules/message/message.module';
+import { ConversationModule } from './modules/conversation/conversation.module';
+import { ParticipantModule } from './modules/participant/participant.module';
 
 @Module({
-  imports: [UserModule, MessageModule, PrismaModule],
+  imports: [
+    UserModule,
+    MessageModule,
+    ConversationModule,
+    ParticipantModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
