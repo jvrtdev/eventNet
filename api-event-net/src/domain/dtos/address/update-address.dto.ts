fbb899547540566a35b5b3238b5569a6f3e5211f
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-03 22:52:29 -0300

    refactor: add class-validator on updateAddressDto

--- api-event-net/src/domain/dtos/address/update-address.dto.ts
+++ api-event-net/src/domain/dtos/address/update-address.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAddressDto } from './create-address.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+  @IsString()
+  @IsNotEmpty()
   id: string;
 }
