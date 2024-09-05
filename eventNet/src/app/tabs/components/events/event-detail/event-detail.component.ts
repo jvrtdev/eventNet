263b94d83053524f0ac56b1ad844f208d0f92499
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-05 20:57:28 -0300

    [mobile] feat: fazendo tela do evento wireframe

--- eventNet/src/app/tabs/components/events/event-detail/event-detail.component.ts
+++ eventNet/src/app/tabs/components/events/event-detail/event-detail.component.ts
@@ -4,19 +4,45 @@ import {
   IonBackButton,
   IonButton,
   IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
   IonContent,
   IonHeader,
   IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonText,
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { arrowBackOutline } from 'ionicons/icons';
+import {
+  addCircle,
+  arrowBackOutline,
+  calendarOutline,
+  cartOutline,
+  locationOutline,
+  removeCircle,
+  ticket,
+  ticketOutline,
+} from 'ionicons/icons';
 
 @Component({
   standalone: true,
   selector: 'event-detail',
   templateUrl: './event-detail.component.html',
   imports: [
+    IonLabel,
+    IonItem,
+    IonList,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+    IonCard,
+    IonText,
     IonButton,
     IonBackButton,
     IonContent,
@@ -24,13 +50,23 @@ import { arrowBackOutline } from 'ionicons/icons';
     IonToolbar,
     IonButtons,
     IonIcon,
+    IonCardSubtitle,
   ],
 })
 export class EventDetailComponent implements OnInit {
   id: any;
+  valorIngresso = '0,00';
+  numberTickets: number = 0;
 
   constructor(private activatedRoute: ActivatedRoute) {
-    addIcons({ arrowBackOutline });
+    addIcons({
+      calendarOutline,
+      locationOutline,
+      cartOutline,
+      ticket,
+      removeCircle,
+      addCircle,
+    });
   }
 
   ngOnInit() {
