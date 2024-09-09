786f566a8656b56405b27f2760f1ca59da0c0a9d
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-09-09 18:01:37 -0300

    refactor: remove decorators

--- api-event-net/src/domain/dtos/address/update-address.dto.ts
+++ api-event-net/src/domain/dtos/address/update-address.dto.ts
@@ -1,9 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAddressDto } from './create-address.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateAddressDto extends PartialType(CreateAddressDto) {
-  @IsString()
-  @IsNotEmpty()
   id: string;
 }
