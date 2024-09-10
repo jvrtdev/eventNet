4e76db2625858c3fe01aa8091fffd82d5a68b471
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-10 20:10:48 -0300

    refactor: add title ...

--- eventNet/src/app/tabs/components/tickets/tab1.page.ts
+++ eventNet/src/app/tabs/components/tickets/tab1.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonText } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.page.html',
   styleUrl: './tab1.page.scss',
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonText, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
   constructor() {}
