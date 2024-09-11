d8e8526a7b674624a4ab8ba56b70e768ed952df8
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-11 10:54:45 -0300

    create: create friendship and conversation in friendship.service

--- api-event-net/src/modules/friendship/services/friendship.service.ts
+++ api-event-net/src/modules/friendship/services/friendship.service.ts
@@ -3,23 +3,40 @@ import { ServiceBase } from 'src/common/base';
 import { CreateFriendshipDto, CreateParticipantDto } from 'src/domain/dtos';
 import { FriendshipEntity } from 'src/domain/entities';
 import { FriendshipRepository } from '../repositories/friendship.repository';
+import { ConversationService } from 'src/modules/conversation/services/conversation.service';
+import { ParticipantService } from 'src/modules/participant/services/participant.service';
 
 @Injectable()
 export class FriendshipService
   implements ServiceBase<FriendshipEntity, CreateFriendshipDto>
 {
-  constructor(private readonly friendshipRepository: FriendshipRepository) {}
+  constructor(
+    private readonly friendshipRepository: FriendshipRepository,
+    private readonly conversationService: ConversationService,
+    private readonly participantService: ParticipantService,
+  ) {}
 
   async create(dto: CreateFriendshipDto): Promise<FriendshipEntity> {
-    const users = [
+    const conversation = await this.conversationService.create();
+
+    dto.conversationId = conversation.id;
+
+    const friendship = await this.friendshipRepository.create(dto);
+
+    const participantData: CreateParticipantDto[] = [
       {
-        userId: dto.userId,
+        conversationId: friendship.conversationId,
+        userId: friendship.userId,
       },
       {
-        userId: dto.friendId,
+        conversationId: friendship.conversationId,
+        userId: friendship.friendId,
       },
     ];
 
-    return await this.friendshipRepository.createFriendshipChat(dto, users);
+    const participant =
+      await this.participantService.createMany(participantData);
+
+    return friendship;
   }
 }
