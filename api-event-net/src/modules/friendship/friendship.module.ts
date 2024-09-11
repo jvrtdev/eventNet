e0df130459bfae9f326070c7326a299fba82da36
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-11 13:45:56 -0300

    feat: add modules conversation and participant on imports

--- api-event-net/src/modules/friendship/friendship.module.ts
+++ api-event-net/src/modules/friendship/friendship.module.ts
@@ -1,12 +1,14 @@
-import { Module } from "@nestjs/common";
-import { FriendshipService } from "./services/friendship.service";
-import { FriendshipRepository } from "./repositories/friendship.repository";
-import { FriendshipController } from "./controllers/friendship.controller";
-
+import { Module } from '@nestjs/common';
+import { FriendshipService } from './services/friendship.service';
+import { FriendshipRepository } from './repositories/friendship.repository';
+import { FriendshipController } from './controllers/friendship.controller';
+import { ConversationModule } from '../conversation/conversation.module';
+import { ParticipantModule } from '../participant/participant.module';
 
 @Module({
+  imports: [ConversationModule, ParticipantModule],
   controllers: [FriendshipController],
   providers: [FriendshipService, FriendshipRepository],
-  exports: [FriendshipService]
+  exports: [FriendshipService],
 })
 export class FriendshipModule {}
