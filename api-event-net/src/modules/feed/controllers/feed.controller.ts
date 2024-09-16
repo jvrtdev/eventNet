15fb92090d1499e2bed7bf69c4f867962e2f099c
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-16 00:13:51 -0300

    feat: add feed module

---
+++ api-event-net/src/modules/feed/controllers/feed.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostEntity } from 'src/domain/entities';
+import { FeedService } from '../services/feed.service';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private readonly feedService: FeedService) {}
+
+  @Get(':userId')
+  getUserFeed(@Param('userId') userId: string): Promise<PostEntity[]> {
+    return this.feedService.getUserFeed(userId);
+  }
+}
---
+++ api-event-net/src/modules/feed/services/feed.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ServiceBase } from 'src/common/base';
+import { PostEntity } from 'src/domain/entities';
+import { FriendshipService } from 'src/modules/friendship/services/friendship.service';
+import { PostService } from 'src/modules/post/services/post.service';
+
+@Injectable()
+export class FeedService {
+  constructor(
+    private readonly postService: PostService,
+    private readonly friendshipService: FriendshipService,
+  ) {}
+
+  async getUserFeed(userId: string): Promise<PostEntity[]> {
+    const friends = await this.friendshipService.getAllFriendsByUserId(userId);
+
+    const friendsId = friends.map((friend) => friend.friendId);
+
+    const postByFriends =
+      await this.postService.findAllPostsByUsersId(friendsId);
+
+    const postsLikedByFriends =
+      await this.postService.findAllPostsLikedByFriends(friendsId);
+
+    const postsCommentedByFriends =
+      await this.postService.findAllPostsCommentedByFriends(friendsId);
+
+    //adicionar repostByFriends
+
+    const combinedPosts = [
+      ...postByFriends,
+      ...postsLikedByFriends,
+      ...postsCommentedByFriends,
+    ];
+
+    // Remover postagens duplicadas
+    const uniquePosts = Array.from(
+      new Set(combinedPosts.map((post) => post.id)),
+    ).map((id) => combinedPosts.find((post) => post.id === id));
+
+    return uniquePosts;
+  }
+}
