5786376f8601f0326279220bb15658f9701ab874
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-16 00:16:38 -0300

    feat: add module  feed

--- api-event-net/src/app.module.ts
+++ api-event-net/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
-import { MessageModule } from './modules/message/message.module';
-import { ConversationModule } from './modules/conversation/conversation.module';
-import { ParticipantModule } from './modules/participant/participant.module';
-import { FriendshipModule } from './modules/friendship/friendship.module';
 import { AddressModule } from './modules/address/address.module';
-import { ProfileModule } from './modules/profile/profile.module';
-import { EventModule } from './modules/event/event.module';
-import { PostModule } from './modules/post/post.module';
-import { LikeModule } from './modules/like/like.module';
 import { CommentModule } from './modules/comment/comment.module';
+import { ConversationModule } from './modules/conversation/conversation.module';
+import { EventModule } from './modules/event/event.module';
+import { FeedModule } from './modules/feed/feed.module';
+import { FriendshipModule } from './modules/friendship/friendship.module';
+import { LikeModule } from './modules/like/like.module';
+import { MessageModule } from './modules/message/message.module';
+import { ParticipantModule } from './modules/participant/participant.module';
+import { PostModule } from './modules/post/post.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { RepostModule } from './modules/repost/repost.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -23,8 +25,10 @@ import { CommentModule } from './modules/comment/comment.module';
     ParticipantModule,
     FriendshipModule,
     PostModule,
+    RepostModule,
     LikeModule,
     CommentModule,
+    FeedModule,
     EventModule,
     PrismaModule,
   ],
