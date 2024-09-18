e26647e26f5c623854b403b3dd2fa6928f8e62de
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-17 23:10:57 -0300

    feat: implement posts feed with api

--- eventNet/src/app/tabs/components/feed/tab2.page.ts
+++ eventNet/src/app/tabs/components/feed/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   IonAvatar,
   IonButton,
@@ -12,6 +12,10 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { chatbubbleOutline, heart, heartOutline, mailOutline, repeat, repeatSharp } from 'ionicons/icons';
+import { FeedService } from './services/feed.service';
+import { PostInterface } from 'src/common/shared/@types/post';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -27,10 +31,26 @@ import { chatbubbleOutline, heart, heartOutline, mailOutline, repeat, repeatShar
     IonTitle,
     IonContent,
     IonIcon,
+    NgFor,
+    NgIf,
+    RouterLink
   ],
 })
-export class Tab2Page {
-  constructor() {
+export class Tab2Page implements OnInit {
+  feed!: PostInterface[]
+  private userId = "e8330aaf-57aa-449a-90fb-8148f94e5596"
+
+  constructor(private readonly feedService: FeedService) {
     addIcons({ chatbubbleOutline, repeatSharp, heartOutline, heart, mailOutline });
   }
+
+  ngOnInit(): void {
+    //getFeedByUserId
+    this.feedService.getFeedByUserId("feed/"+ this.userId).subscribe((feed: PostInterface[]) => {
+      this.feed = feed
+      console.log(feed)
+    })
+  }
+
+  
 }
