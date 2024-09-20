fbe1a69cc3dfaad79072924ab66caad08d39aa75
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-09-19 23:26:50 -0300

    cleanup

--- eventNet/src/app/app.routes.ts
+++ eventNet/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { EventDetailComponent } from './tabs/components/events/event-detail/event-detail.component';
+import { UserPreferencesComponent } from 'src/app/user-preferences/user-preferences.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { ChatContainerComponent } from './chat-container/chat-container.component';
 import { ChatComponent } from './chat-container/chat/chat.component';
-import { UserPreferencesComponent } from 'src/app/user-preferences/user-preferences.component';
-import { EditAccountComponent } from './user-preferences/components/edit-account/edit-account.component';
-import { DarkmodePreferencesComponent } from './user-preferences/components/darkmode-preferences/darkmode-preferences.component';
-import { PrivacyPreferencesComponent } from './user-preferences/components/privacy-preferences/privacy-preferences.component';
+import { EventDetailComponent } from './tabs/components/events/event-detail/event-detail.component';
 import { PostDetailsComponent } from './tabs/components/feed/components/post-details/post-details.component';
+import { DarkmodePreferencesComponent } from './user-preferences/components/darkmode-preferences/darkmode-preferences.component';
+import { EditAccountComponent } from './user-preferences/components/edit-account/edit-account.component';
+import { PrivacyPreferencesComponent } from './user-preferences/components/privacy-preferences/privacy-preferences.component';
 
 export const routes: Routes = [
   {
@@ -23,7 +24,7 @@ export const routes: Routes = [
   },
   {
     path: 'chat/:id',
-    component: ChatComponent
+    component: ChatComponent,
   },
   {
     path: 'preferences',
@@ -31,18 +32,18 @@ export const routes: Routes = [
   },
   {
     path: 'preferences/account',
-    component: EditAccountComponent
+    component: EditAccountComponent,
   },
   {
     path: 'preferences/theme',
-    component: DarkmodePreferencesComponent
+    component: DarkmodePreferencesComponent,
   },
   {
     path: 'preferences/privacy',
-    component: PrivacyPreferencesComponent
+    component: PrivacyPreferencesComponent,
   },
   {
     path: 'post/:id',
-    component: PostDetailsComponent
-  }
+    component: PostDetailsComponent,
+  },
 ];
