c873ae350a0b9f0a0f41d2990eae5ab95bbe87eb
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-09-25 20:19:08 -0300

    cleanUp

--- api-event-net/src/domain/dtos/profile/update-profile.dto.ts
+++ api-event-net/src/domain/dtos/profile/update-profile.dto.ts
@@ -1,6 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProfileDto } from './create-profile.dto';
-import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateProfileDto extends PartialType(CreateProfileDto) {
   id: string;
