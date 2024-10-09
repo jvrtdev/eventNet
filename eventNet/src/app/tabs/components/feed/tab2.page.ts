d3265a4521a05ba3635fe42f61233b61d3d77a82
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-10-09 10:44:33 -0300

    feat: add fab button

--- eventNet/src/app/tabs/components/feed/tab2.page.ts
+++ eventNet/src/app/tabs/components/feed/tab2.page.ts
@@ -1,9 +1,15 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { getUserId } from '@core/common/utils/getUserId';
+import { PostInterface } from '@core/shared/@types/post';
 import {
   IonAvatar,
   IonButton,
   IonButtons,
   IonContent,
+  IonFab,
+  IonFabButton,
   IonHeader,
   IonIcon,
   IonText,
@@ -11,47 +17,60 @@ import {
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chatbubbleOutline, heart, heartOutline, mailOutline, repeat, repeatSharp } from 'ionicons/icons';
+import {
+  add,
+  chatbubbleOutline,
+  heart,
+  heartOutline,
+  mailOutline,
+  repeat,
+  repeatSharp,
+} from 'ionicons/icons';
 import { FeedService } from './services/feed.service';
-import { PostInterface } from '@core/shared/@types/post';
-import { NgFor, NgIf } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { getUserId } from '@core/common/utils/getUserId';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   standalone: true,
   imports: [
+    IonFab,
     IonButtons,
     IonButton,
     IonText,
     IonAvatar,
     IonHeader,
+    IonFabButton,
     IonToolbar,
     IonTitle,
     IonContent,
     IonIcon,
     NgFor,
     NgIf,
-    RouterLink
+    RouterLink,
   ],
 })
 export class Tab2Page implements OnInit {
-  feed!: PostInterface[]
-  private userId = getUserId()
+  feed!: PostInterface[];
+  private userId = getUserId();
 
   constructor(private readonly feedService: FeedService) {
-    addIcons({ chatbubbleOutline, repeatSharp, heartOutline, heart, mailOutline });
+    addIcons({
+      chatbubbleOutline,
+      repeatSharp,
+      heartOutline,
+      heart,
+      mailOutline,
+      add,
+    });
   }
 
   ngOnInit(): void {
     //getFeedByUserId
-    this.feedService.getFeedByUserId("feed/"+ this.userId).subscribe((feed: PostInterface[]) => {
-      this.feed = feed
-      console.log(feed)
-    })
+    this.feedService
+      .getFeedByUserId('feed/' + this.userId)
+      .subscribe((feed: PostInterface[]) => {
+        this.feed = feed;
+        console.log(feed);
+      });
   }
-
-  
 }
