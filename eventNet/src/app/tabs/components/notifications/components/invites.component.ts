9225fc96c7a704bc17b033c875d1261276a6bd2d
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-04 13:33:35 -0300

    feat: acceptPendingInvite and refusePendingInvite methods

--- eventNet/src/app/tabs/components/notifications/components/invites.component.ts
+++ eventNet/src/app/tabs/components/notifications/components/invites.component.ts
@@ -2,6 +2,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ConversationInterface } from '@core/shared/@types/conversation';
+import { ToastComponent } from '@core/shared/components/toast.component';
 import {
   IonAvatar,
   IonIcon,
@@ -10,6 +11,7 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { checkmarkCircleOutline, closeCircleOutline } from 'ionicons/icons';
+import { NotificationsService } from '../notifications.service';
 
 @Component({
   standalone: true,
@@ -18,23 +20,32 @@ import { checkmarkCircleOutline, closeCircleOutline } from 'ionicons/icons';
   template: `
     <div *ngIf="invites">
       <div
-        class="flex items-center justify-between w-full"
+        class="flex items-center justify-between w-full border-y border-slate-300"
         *ngFor="let invite of invites"
       >
-        <div class="flex gap-1 w-full">
+        <div class="flex items-center gap-1 w-full">
           <ion-avatar slot="start">
             <img
               alt="Profile person avatar"
-              class="w-12 h-12 min-h-fit min-w-fit rounded-full "
+              class="w-12 h-12 min-h-fit min-w-fit rounded-full border "
+              src="{{ invite.recipient?.profile?.avatar }}"
             />
           </ion-avatar>
           <ion-label class="">
-            {{ invite.isGroup }}
+            {{ invite.recipient?.name }}
           </ion-label>
         </div>
         <div class="flex items-center gap-2 text-3xl">
-          <ion-icon name="checkmark-circle-outline" color="success"></ion-icon>
-          <ion-icon name="close-circle-outline" color="danger"></ion-icon>
+          <ion-icon
+            name="checkmark-circle-outline"
+            color="success"
+            (click)="acceptInvite(invite)"
+          ></ion-icon>
+          <ion-icon
+            name="close-circle-outline"
+            color="danger"
+            (click)="refuseInvite(invite)"
+          ></ion-icon>
         </div>
       </div>
     </div>
@@ -43,9 +54,53 @@ import { checkmarkCircleOutline, closeCircleOutline } from 'ionicons/icons';
 export class InvitesComponent {
   @Input() invites!: ConversationInterface[];
 
-  constructor() {
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private toast: ToastComponent
+  ) {
     addIcons({ checkmarkCircleOutline, closeCircleOutline });
-    // this.invites.filter((invite) => invite.participant[1]);
-    // console.log(this.invites);
+  }
+
+  acceptInvite(invite: ConversationInterface) {
+    this.notificationsService.acceptPendingInvite(invite.id).subscribe({
+      next: (response) => {
+        this.toast.setToast({
+          label: 'Convite aceito com sucesso!',
+          icon: 'checkbox',
+          color: 'success',
+        });
+        console.log(response);
+      },
+
+      error: (error) => {
+        this.toast.setToast({
+          label: error.message,
+          icon: 'close-circle',
+          color: 'danger',
+        });
+        console.log(error);
+      },
+    });
+  }
+  refuseInvite(invite: ConversationInterface) {
+    this.notificationsService.refusedPendingInvite(invite.id).subscribe({
+      next: (response) => {
+        this.toast.setToast({
+          label: 'Convite rejeitado com sucesso!',
+          icon: 'checkbox',
+          color: 'success',
+        });
+        console.log(response);
+      },
+
+      error: (error) => {
+        this.toast.setToast({
+          label: error.message,
+          icon: 'close-circle',
+          color: 'danger',
+        });
+        console.log(error);
+      },
+    });
   }
 }
