1598531a0962ad4e695e854e1175f7b1df3c7e49
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-14 09:05:01 -0300

    CONFIG: DEEPLINK

--- eventNet/src/app/app.component.ts
+++ eventNet/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { ToastComponent } from "../core/shared/components/toast.component";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { App } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
+import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
+import { ToastComponent } from '../core/shared/components/toast.component';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,17 @@ import { ToastComponent } from "../core/shared/components/toast.component";
   standalone: true,
   imports: [IonApp, IonRouterOutlet, ToastComponent],
 })
-export class AppComponent{}
+export class AppComponent implements OnInit {
+  constructor(private platform: Platform, private router: Router) {}
+  ngOnInit(): void {
+    this.platform.ready().then(() => {
+      if (Capacitor.isNativePlatform()) {
+        App.addListener('appUrlOpen', (event: any) => {
+          const url = event.url;
+          const path = url.split('://')[1];
+          this.router.navigate([`/${path}`]);
+        });
+      }
+    });
+  }
+}
