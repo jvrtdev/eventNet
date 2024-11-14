442889c76073c906d943037ec75ce9fc39c7ebf5
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-14 09:06:07 -0300

    add capacitor for build apk

--- eventNet/capacitor.config.ts
+++ eventNet/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'eventNet',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'eventNet',
+  },
 };
 
 export default config;
