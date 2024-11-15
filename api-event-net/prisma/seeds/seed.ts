f3e26729f25f55c132432a0f2fc0e031943edaa9
joaoromanodev <joaovitorromanoteixeira@gmail.com>
2024-11-15 14:35:13 -0300

    refactor: seed

---
+++ api-event-net/prisma/seeds/seed.ts
@@ -0,0 +1,124 @@
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+import generateQrCode from '../../src/common/utils/qrcode/generateQrCode';
+const prisma = new PrismaClient();
+
+type DataType = {
+  id?: string;
+  name: string;
+  userName: string;
+  email: string;
+  password: string;
+  phone: string;
+  profile: {
+    bio: string;
+    avatar: string;
+    github: string;
+    linkedin: string;
+    instagram: string;
+  };
+  address: {
+    city: string;
+    state: string;
+    neighborhood: string;
+  };
+};
+
+async function dataSeed() {
+  for (let i = 0; i < 50; i++) {
+    const user = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        userName: faker.internet.username(),
+        password: 'oi',
+        phone: faker.phone.number(),
+        address: {
+          create: {
+            city: faker.location.city(),
+            state: faker.location.state(),
+            neighborhood: faker.location.street(),
+          },
+        },
+        profile: {
+          create: {
+            avatar: faker.image.avatar(),
+            bio: faker.person.jobTitle(),
+            github: faker.internet.url(),
+            instagram: faker.internet.url(),
+            linkedin: faker.internet.url(),
+          },
+        },
+        userPosts: {
+          create: {
+            text: faker.lorem.paragraph(),
+          },
+        },
+      },
+    });
+    const event = await prisma.event.create({
+      data: {
+        title: faker.book.title(),
+        description: faker.lorem.text(),
+        start_datetime: faker.date.future(),
+        end_datetime: faker.date.future(),
+        location: faker.location.city(),
+        qr_code: '',
+        image: '',
+        userEvent: {
+          create: {
+            user: {
+              connect: {
+                id: user.id,
+                role: 'common',
+              },
+            },
+            role: 'owner',
+          },
+        },
+        conversation: {
+          create: {
+            isGroup: true,
+            status: 'accepted',
+            participant: {
+              create: {
+                user: {
+                  connect: {
+                    id: user.id,
+                  },
+                },
+                role: 'admin',
+              },
+            },
+            message: {
+              create: {
+                content: 'Bem vindos ao chat do evento',
+                user: {
+                  connect: {
+                    id: user.id,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    await prisma.event.update({
+      where: {
+        id: event.id,
+      },
+      data: {
+        qr_code: generateQrCode(event.id),
+      },
+    });
+
+    console.log('Event created:', i, event);
+    console.log('User created:', i, user);
+  }
+}
+
+dataSeed().then(() => {
+  console.log('User seeded');
+  prisma.$disconnect();
+});
--- api-event-net/prisma/seeds/user.ts
+++ api-event-net/prisma/seeds/user.ts
@@ -1,146 +1,60 @@
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
-const userData = [
-  {
-    id: '36a57eae-1a36-4d2a-b5e7-3c6199d52b5a',
-    name: 'Ana Silva',
-    userName: 'ana.silva',
-    email: 'ana.silva@example.com',
-    password: '$2b$10$3XELlt3h6NTwSopOV1oixOBt.T5wCfGtIykaO9WzVy0JmpqhgNzoC',
-    phone: '+55 11 91234-5678',
-  },
-  {
-    id: '91a9f41b-4782-4efb-95b8-b167a14e5a94',
-    name: 'Carlos Oliveira',
-    userName: 'carlos.oliveira',
-    email: 'carlos.oliveira@example.com',
-    password: '$2b$10$3XELlt3h6NTwSopOV1oixOBt.T5wCfGtIykaO9WzVy0JmpqhgNzoC',
-    phone: '+55 21 98765-4321',
-  },
-  {
-    id: 'fc52e994-5827-4eb1-b8f3-f5b4f7c7e91f',
-    name: 'Mariana Costa',
-    userName: 'mariana.costa',
-    email: 'mariana.costa@example.com',
-    password: '$2b$10$3XELlt3h6NTwSopOV1oixOBt.T5wCfGtIykaO9WzVy0JmpqhgNzoC',
-    phone: '+55 31 99876-5432',
-  },
-  {
-    id: 'c4c02145-60c9-46ae-9152-30df6f6a7b8a',
-    name: 'Rafael Santos',
-    userName: 'rafael.santos',
-    email: 'rafael.santos@example.com',
-    password: '$2b$10$3XELlt3h6NTwSopOV1oixOBt.T5wCfGtIykaO9WzVy0JmpqhgNzoC',
-    phone: '+55 19 99765-4321',
-  },
-  {
-    id: 'c88b0d74-5a70-41fa-8df2-78b9e6f7af2e',
-    name: 'Juliana Almeida',
-    userName: 'juliana.almeida',
-    email: 'juliana.almeida@example.com',
-    password: '$2b$10$3XELlt3h6NTwSopOV1oixOBt.T5wCfGtIykaO9WzVy0JmpqhgNzoC',
-    phone: '+55 41 99654-3210',
-  },
-];
+type DataType = {
+  id?: string;
+  name: string;
+  userName: string;
+  email: string;
+  password: string;
+  phone: string;
+  profile: {
+    bio: string;
+    avatar: string;
+    github: string;
+    linkedin: string;
+    instagram: string;
+  };
+  address: {
+    city: string;
+    state: string;
+    neighborhood: string;
+  };
+};
 
-const profileData = [
-  {
-    bio: 'Desenvolvedor Full Stack',
-    avatar: 'https://example.com/avatar1.jpg',
-    github: 'https://github.com/usuario1',
-    linkedin: 'https://linkedin.com/in/usuario1',
-    instagram: 'https://instagram.com/usuario1',
-    userId: '36a57eae-1a36-4d2a-b5e7-3c6199d52b5a',
-    createdAt: '2024-09-19T21:26:10.589Z',
-  },
-  {
-    bio: 'Designer Gráfico',
-    avatar: 'https://example.com/avatar2.jpg',
-    github: null,
-    linkedin: 'https://linkedin.com/in/usuario2',
-    instagram: 'https://instagram.com/usuario2',
-    userId: '91a9f41b-4782-4efb-95b8-b167a14e5a94',
-    createdAt: '2024-09-19T21:26:10.589Z',
-  },
-  {
-    bio: 'Analista de Dados',
-    avatar: 'https://example.com/avatar3.jpg',
-    github: 'https://github.com/usuario3',
-    linkedin: 'https://linkedin.com/in/usuario3',
-    instagram: null,
-    userId: 'fc52e994-5827-4eb1-b8f3-f5b4f7c7e91f',
-    createdAt: '2024-09-19T21:26:10.589Z',
-  },
-  {
-    bio: 'Gerente de Projetos',
-    avatar: null,
-    github: 'https://github.com/usuario4',
-    linkedin: 'https://linkedin.com/in/usuario4',
-    instagram: 'https://instagram.com/usuario4',
-    userId: 'c4c02145-60c9-46ae-9152-30df6f6a7b8a',
-    createdAt: '2024-09-19T21:26:10.589Z',
-  },
-  {
-    bio: 'Estudante de Ciência da Computação',
-    avatar: 'https://example.com/avatar5.jpg',
-    github: null,
-    linkedin: null,
-    instagram: 'https://instagram.com/usuario5',
-    userId: 'c88b0d74-5a70-41fa-8df2-78b9e6f7af2e',
-    createdAt: '2024-09-19T21:26:10.589Z',
-  },
-];
-
-const addresseData = [
-  {
-    city: 'São Paulo',
-    state: 'SP',
-    neighborhood: 'Jardins',
-    userId: '36a57eae-1a36-4d2a-b5e7-3c6199d52b5a',
-  },
-  {
-    city: 'Rio de Janeiro',
-    state: 'RJ',
-    neighborhood: 'Copacabana',
-    userId: '91a9f41b-4782-4efb-95b8-b167a14e5a94',
-  },
-  {
-    city: 'Belo Horizonte',
-    state: 'MG',
-    neighborhood: 'Savassi',
-    userId: 'fc52e994-5827-4eb1-b8f3-f5b4f7c7e91f',
-  },
-  {
-    city: 'Curitiba',
-    state: 'PR',
-    neighborhood: 'Centro',
-    userId: 'c4c02145-60c9-46ae-9152-30df6f6a7b8a',
-  },
-  {
-    city: 'Porto Alegre',
-    state: 'RS',
-    neighborhood: 'Cidade Baixa',
-    userId: 'c88b0d74-5a70-41fa-8df2-78b9e6f7af2e',
-  },
-];
-
-async function userSeed() {
-  await prisma.user.createMany({
-    data: userData,
-  });
-
-  await prisma.profile.createMany({
-    data: profileData,
-  });
-
-  await prisma.address.createMany({
-    data: addresseData,
-  });
+async function dataSeed() {
+  for (let i = 0; i < 50; i++) {
+    const data = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        userName: faker.internet.username(),
+        password: 'oi',
+        phone: faker.phone.number(),
+        address: {
+          create: {
+            city: faker.location.city(),
+            state: faker.location.state(),
+            neighborhood: faker.location.street(),
+          },
+        },
+        profile: {
+          create: {
+            avatar: faker.image.avatar(),
+            bio: faker.person.jobTitle(),
+            github: faker.internet.url(),
+            instagram: faker.internet.url(),
+            linkedin: faker.internet.url(),
+          },
+        },
+      },
+    });
+    console.log('User created:', i, data);
+  }
 }
 
-userSeed().then(() => {
+dataSeed().then(() => {
   console.log('User seeded');
   prisma.$disconnect();
 });
